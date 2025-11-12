7953d15177b6908125871d60f1a4c82942168b4b
shivam182004 <shivamjoshiofficial08@gmail.com>
2025-11-12 23:20:21 +0530

    upadated the datae and location

--- src/components/homePage/HeroSection.vue
+++ src/components/homePage/HeroSection.vue
@@ -29,12 +29,12 @@
               <line x1="8" y1="2" x2="8" y2="6"></line>
               <line x1="3" y1="10" x2="21" y2="10"></line>
             </svg>
-            <span>15 December 2025</span>
+            <span>20 December 2025</span>
             <svg class="info-icon info-icon-spacing" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
               <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path>
               <circle cx="12" cy="10" r="3"></circle>
             </svg>
-            <span class="location-link">Sayaji Indore</span>
+            <span class="location-link">Marriott Indore</span>
           </div>
         </div>
 
